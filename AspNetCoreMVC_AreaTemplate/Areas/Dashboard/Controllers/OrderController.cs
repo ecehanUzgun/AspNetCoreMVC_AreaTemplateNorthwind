684e01c7fa6403824f949a364b5f73d4aab69c60
ecehanUzgun <ecehan766@gmail.com>
2024-10-06 08:27:55 +0300

    order details hatalı

---
+++ AspNetCoreMVC_AreaTemplate/Areas/Dashboard/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using AspNetCoreMVC_AreaTemplate.Models.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVC_AreaTemplate.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class OrderController : Controller
+    {
+        private readonly NorthwindContext _context;
+        public OrderController(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var orders = _context.Orders.ToList();
+            return View(orders);
+        }
+    }
+}
